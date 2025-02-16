ed4160b207694686853fae1d8ab34bdda9692c00
Yohoji <etha3leb@gmail.com>
2025-02-16 12:24:14 +0100

    Setup my script to hangup a signal call after 1h, also setup a semi secure way of unlocking my screen without hardcoding my password in the script

---
+++ hangup-signal.sh
@@ -0,0 +1,21 @@
+# Import decryption key and decrypt password
+gpg --import D:/keys/private.key
+password=$(gpg --decrypt ./encrypted-passowrd.gpg)
+
+# Wait for 1 hour (3600 seconds)
+# sleep 3600
+
+# open screen
+adb shell input keyevent 26
+
+# swipe to get password screen
+adb shell input swipe 500 1000 500 500
+
+# Enter my password to unlock
+adb shell input text $password
+
+# Press enter / submit password
+adb shell input keyevent 66
+
+# Tap on X Y coordiantes (i.e hangup signal)
+adb shell input tap 562 1335
